04857fec4a7eca60cdd0f318e5a681e462675e19
EngineerYo <willcrain96@gmail.com>
2020-02-05 09:51:29 -0800

    Finding pupils by taking blob of whole eye region

--- getEyes.py
+++ getEyes.py
@@ -24,6 +24,8 @@ os.chdir(r'C:\Users\willc\AppData\Local\Packages\PythonSoftwareFoundation.Python
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+
 
 #       #   Functions   #       #
 
@@ -32,9 +34,23 @@ def getEyes(inFrame):
     
     return eyes
 
+def getEyeFrames(inFrame, eyes):
+    eyeFrames = []
+    for (x, y, w, h) in eyes:
+        eyeFrames.append(inFrame[x:x+w, y:y+h])
+
+    return eyeFrames
+
 def drawEyes(inFrame, eyes):
     for (x, y, w, h) in eyes:
-        cv2.rectangle(inFrame, (x, y), (x+w, y+h), (0, 0, 0), 1)
+        pass
+        #cv2.rectangle(inFrame, (x, y), (x+w, y+h), (0, 0, 0), 1)
+
+    return inFrame
+
+def drawPupils(inFrame, pupils):
+    for (x, y, w, h) in pupils:
+        cv2.circle(inFrame, (x, y), 3, (0, 0, 255, 127), -1)
 
     return inFrame
 
@@ -42,14 +58,48 @@ def drawEyes(inFrame, eyes):
 #       #    Execute    #       #
 
 while True:
-    ret, frameC = cap.read()
+#    ret, frameC = cap.read()
+    frameC = cv2.imread(r'C:\Users\willc\OneDrive\Documents\GitHub\Computer-Vision\testGetEyes.png')
     frameG = cv2.cvtColor(frameC, cv2.COLOR_RGB2GRAY)
     frameG = np.uint8(frameG)
+
+    outFrame = np.copy(frameC)
     
     eyes = getEyes(frameG)
-    outFrame = drawEyes(frameC, eyes)
+    eyeFrames = getEyeFrames(frameG, eyes)
     
-    cv2.imshow('Frame', outFrame)
+    outFrame = drawEyes(frameC, eyes)
+
+    side = 0
+    for (x, y, w, h) in eyes:
+        eyeFrame = frameG[y:y+h, x:x+w]
+        eyeFrame = cv2.equalizeHist(eyeFrame)
+        eyeFrame = cv2.morphologyEx(eyeFrame, cv2.MORPH_OPEN, kernel)
+
+        threshold = cv2.inRange(eyeFrame, 5, 10)
+        contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in contours:
+            print([c])
+            #cv2.drawContours(outFrame, [c], 0, (0, 0, 0), 1)
+    
+        lCon = None
+        if len(contours) > 1:
+            maxArea = 0
+            for c in contours:
+                area = cv2.contourArea(c)
+                if area > maxArea:
+                    maxArea = area
+                    lCon = c
+
+        if lCon is not None:
+            center = cv2.moments(c)
+            if not center['m00'] == 0.0:
+                cx, cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
+                cv2.circle(outFrame, (cx+x, cy+y), 5, (0, 0, 255), 1)
+                
+    side = side + 1
+    cv2.imshow('outFrame', outFrame)
     
     k = cv2.waitKey(1)
     if k == 27 or k == ord('q'):
